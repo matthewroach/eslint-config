1d6e346308d09ef5a914dfc8ef8fadbcd3e645f0
Matthew Roach <m@roach.is>
2024-05-22 14:44:56 +0100

    Add basic eslint package structure and config

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+	ignorePatterns: [
+		'.generated/**/*',
+		'**/*.d.ts',
+		'**/*.js',
+		'*.targets',
+		'**/temp/*',
+	],
+	rules: {
+		'@next/next/no-img-element': 'off', // Don't force next/image
+		'jsx-a11y/alt-text': ['warn', { elements: ['img'] }], // Don't force alt for <Image/> (sourced from Sitecore media)
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@next/next/no-html-link-for-pages': ['error', './src/hcamain/src/pages/'],
+		'react/no-children-prop': 'off',
+	},
+}
